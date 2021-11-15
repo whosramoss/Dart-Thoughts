12f7fdac9fb9b67aaa089246b860028a973871f0
Gabriel Ramos <whosramoss@gmail.com>
2021-11-15 14:57:30 -0300

    Update iso-format.dart

--- iso-format.dart
+++ iso-format.dart
@@ -2,39 +2,35 @@
 import 'package:meta/meta.dart';
 
 void main() {
+  print('Converts DateTime to ISO format');
   print(DateUtils.formatISOTime(DateTime.now()));
-
+  print('---');
+  print('Get ISO time String from DateTime');
   print(DateUtils.getCurrentISOTimeString());
 }
 
 class DateUtils {
  
- ///Converts DateTime to ISO format 
- ///Output: 2020-09-16T20:41:09.331+05:30
   static String formatISOTime(DateTime date) {
     var duration = date.timeZoneOffset;
-    if (duration.isNegative) {
-     return (date.toIso8601String() +
-          "-${duration.inHours.toString().padLeft(2, '0')}:${(duration.inMinutes - (duration.inHours * 60)).toString().padLeft(2, '0')}");
-    }
-     
-    else
-      return (date.toIso8601String() +
-          "+${duration.inHours.toString().padLeft(2, '0')}:${(duration.inMinutes - (duration.inHours * 60)).toString().padLeft(2, '0')}");
+    var _date = date.toIso8601String();
+    var _hour = duration.inHours.toString().padLeft(2, '0');
+    var _min = (duration.inMinutes - (duration.inHours * 60)).toString().padLeft(2, '0');
+    return duration.isNegative ? (_date + "-$_hour:$_min") : (_date + "+$_hour:$_min")
+     ///Example Output: 2020-09-16T20:41:09.331+05:30
   }
 
- ///get ISO time String from DateTime
- ///Output: 2020-09-16T20:42:38.629+05:30
   static String getCurrentISOTimeString({DateTime dateTime}) {
     var date = dateTime ?? DateTime.now();
     //Time zone may be null in dateTime hence get timezone by  datetime
     var duration = DateTime.now().timeZoneOffset;
-    if (duration.isNegative)
-      //TODO: convert duration to abs value instead of below params
-      return (date.toIso8601String() +
-          "-${duration.inHours.abs().toString().padLeft(2, '0')}:${(duration.inMinutes.abs() - (duration.inHours.abs() * 60)).toString().padLeft(2, '0')}");
-    else
-      return (date.toIso8601String() +
-          "+${duration.inHours.toString().padLeft(2, '0')}:${(duration.inMinutes - (duration.inHours * 60)).toString().padLeft(2, '0')}");
+    var _date = date.toIso8601String();
+    var _hour = duration.inHours.toString().padLeft(2, '0');
+    var _hourAbs = duration.inHours.abs().toString().padLeft(2, '0');
+    var _min = (duration.inMinutes - (duration.inHours * 60)).toString().padLeft(2, '0');
+    var _minAbs = (duration.inMinutes.abs()  - (duration.inHours.abs()  * 60)).toString().padLeft(2, '0');
+    return duration.isNegative ?(_date + "-$_hourAbs:$_minAbs"): (_date + "+$_hour:$_min")
+      /// Example Output: 2020-09-16T20:42:38.629+05:30
   }
+  
 }
